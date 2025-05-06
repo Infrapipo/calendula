5d5941a43665f712a5a95611dfc2641de6e13af9
Valentín Rodríguez <166716947+Infrapipo@users.noreply.github.com>
2025-05-06 18:37:17 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// Mensaje de bienvenida en consola
+console.log("Bienvenido a Caléndula Artesanal 🌼");
+
+// Ejemplo: mostrar una alerta cuando se carga la página
+window.addEventListener("load", () => {
+  console.log("Página cargada completamente.");
+});
